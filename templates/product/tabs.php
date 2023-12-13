a22b792dd1783d70afa9b5f0fa804ca9efc62431
Elena Kiseljova <elena.ki@webest.ru>
2023-12-13 13:45:46 +0200

    upd: small change

--- templates/product/tabs.php
+++ templates/product/tabs.php
@@ -1,60 +1,50 @@
-<?php 
-  global $class;
+<?php
+global $class;
 
-  if ( !isset($class) ) {
-    $class = '';
-  }
+if (!isset($class)) {
+  $class = '';
+}
 
-  $title = get_sub_field( 'title' ) ?? '';
+$title = get_sub_field('title') ?? '';
 
-  $list = get_sub_field( 'list' );
+$list = get_sub_field('list');
 
-  $button = get_sub_field( 'button' );
+$button = get_sub_field('button');
 
-  $shops_page_url = bob_get_special_page( 'shops', 'url' ) ?? get_permalink(  );
+$shops_page_url = bob_get_special_page('shops', 'url') ?? get_permalink();
 
-  $selected = '';
+$selected = '';
 ?>
 <div class="tabs" id="tabs">
-  <?php if ( $class === 'stripes' || $class === 'sweets' ) : ?>
-    <img
-      class="icon composition__fruit composition__fruit--right"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/grass.svg"
-      alt="<?=get_bloginfo( 'name' ); ?>"
-    />
-  <?php elseif ( $class === 'marmalade' ) : ?>
-    <img
-      class="icon composition__fruit composition__fruit--left"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/banana.svg"
-      alt="<?=get_bloginfo( 'name' ); ?>"
-    />
-  <?php elseif( $class === 'smoothies' ) : ?>
-    <img
-      class="icon composition__fruit composition__fruit--left"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/banana.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
-  <?php elseif( $class === 'chief' ) : ?>
-    <img
-      class="icon composition__fruit composition__fruit--left"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/orange.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
+  <?php if ($class === 'stripes' || $class === 'sweets') : ?>
+    <img class="icon composition__fruit composition__fruit--right" src="<?= get_template_directory_uri(); ?>/assets/img/icons/grass.svg" alt="<?= get_bloginfo('name'); ?>" />
+  <?php elseif ($class === 'marmalade') : ?>
+    <img class="icon composition__fruit composition__fruit--left" src="<?= get_template_directory_uri(); ?>/assets/img/icons/banana.svg" alt="<?= get_bloginfo('name'); ?>" />
+  <?php elseif ($class === 'smoothies') : ?>
+    <img class="icon composition__fruit composition__fruit--left" src="<?= get_template_directory_uri(); ?>/assets/img/icons/banana.svg" alt="<?= get_bloginfo('name'); ?>" />
+  <?php elseif ($class === 'chief') : ?>
+    <img class="icon composition__fruit composition__fruit--left" src="<?= get_template_directory_uri(); ?>/assets/img/icons/orange.svg" alt="<?= get_bloginfo('name'); ?>" />
   <?php endif; ?>
-  
+
 
   <h2 class="composition__title title title--width">
     <?= $title; ?>
   </h2>
 
-  <?php if ( $list && !empty($list) && is_array($list) && !is_wp_error( $list ) ) : ?>
-    <?php if ( count($list) > 1 ) : ?>
+  <?php if ($list && !empty($list) && is_array($list) && !is_wp_error($list)) : ?>
+    <?php
+    // Отфильтровываю единицы скрытые из админки
+    $list = array_filter($list, function ($item) {
+      return $item['products_show'] === true;
+    })
+    ?>
+    <?php if (count($list) > 1) : ?>
       <div class="tabs__btns">
         <?php foreach ($list as $key => $item) : ?>
-          <?php 
-            if ( $key === 0 ) {
-              $selected = $item['text_big'] . $item['text_small'];
-            }  
+          <?php
+          if ($key === 0) {
+            $selected = $item['text_big'] . $item['text_small'];
+          }
           ?>
           <button class="tabs__btn <?= $key === 0 ? 'active' : ''; ?>" data-tab="<?= $key + 1; ?>">
             <span class="tabs__number"><?= $item['text_big'] ?? ''; ?></span><?= $item['text_small'] ?? ''; ?>
@@ -63,25 +53,14 @@
       </div>
 
       <div class="tabs__select select" id="select-2">
-        <button
-          type="button"
-          class="select__toggle"
-          data-select="toggle"
-          data-index="-1"
-        >
+        <button type="button" class="select__toggle" data-select="toggle" data-index="-1">
           <span><?= $selected; ?></span>
         </button>
 
         <div class="select__dropdown">
           <ul class="select__options">
             <?php foreach ($list as $key => $item) : ?>
-              <li
-                class="select__option tabs__btn-select"
-                data-select="option"
-                data-value="<?= $item['text_big'] ?? ''; ?>"
-                data-index="<?= $key + 1; ?>"
-                data-tab="<?= $key + 1; ?>"
-              >
+              <li class="select__option tabs__btn-select" data-select="option" data-value="<?= $item['text_big'] ?? ''; ?>" data-index="<?= $key + 1; ?>" data-tab="<?= $key + 1; ?>">
                 <span><?= $item['text_big'] ?? ''; ?><?= $item['text_small'] ?? ''; ?></span>
               </li>
             <?php endforeach; ?>
@@ -92,69 +71,66 @@
 
     <div class="tabs__contents">
       <?php foreach ($list as $key => $item) : ?>
-        <?php 
-          $products = $item['products'] ?? [];
+        <?php
+        $products = $item['products'] ?? [];
         ?>
         <div class="tabs__content content <?= $key === 0 ? 'active' : ''; ?>" data-content="<?= $key + 1; ?>">
-          <?php if ( $products && !empty($products) && is_array($products) && !is_wp_error( $products ) ) : ?>
+          <?php if ($products && !empty($products) && is_array($products) && !is_wp_error($products)) : ?>
             <ul class="content__list">
               <?php foreach ($products as $key => $product) : ?>
-                <?php 
-                  $image = $product['image'] ?? '';
+                <?php
+                $image = $product['image'] ?? '';
 
-                  $title = $product['title'] ?? '';
-                  
-                  $subtitle = $product['subtitle'] ?? '';
+                $title = $product['title'] ?? '';
 
-                  $long_ingredients = $product['long_ingredients'];
-                ?> 
+                $subtitle = $product['subtitle'] ?? '';
+
+                $long_ingredients = $product['long_ingredients'];
+                ?>
                 <li class="content__item">
-                  <?php if ( $long_ingredients ) : ?>
+                  <?php if ($long_ingredients) : ?>
                     <div class="content__wrapper">
-                  <?php endif; ?>
+                    <?php endif; ?>
 
-                  <div class="content__link">
-                    <div class="content__image">
-                      <img
-                        src="<?= $image; ?>"
-                        alt="<?= strip_tags( $title ); ?>"
-                      />
-                    </div>
-                    <h2 class="content__title"><?= $title ;?></h2>
-
-                    <?php if ( $long_ingredients ) : ?>
-                      <div class="content__bottom">
-                        <span class="content__label"><?= __( 'Дивитися інгредієнти', 'bob' ); ?></span>
-                        <button class="content__btn">
-                          <svg width="36" height="36">
-                            <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#eye"></use>
-                          </svg>
-                        </button>
+                    <div class="content__link">
+                      <div class="content__image">
+                        <img src="<?= $image; ?>" alt="<?= strip_tags($title); ?>" />
                       </div>
-                    <?php else : ?>
-                      <p class="content__descr"><?= $subtitle ;?></p>
-                    <?php endif; ?>                    
-                  </div>
+                      <h2 class="content__title"><?= $title; ?></h2>
 
-                  <?php if ( $long_ingredients ) : ?>
-                    <div class="content__ingredients">
-                      <h3><?= __( 'Інгредієнти:', 'bob' ); ?></h3>
-                      <p>
-                        <?= $subtitle ;?>
-                      </p>
+                      <?php if ($long_ingredients) : ?>
+                        <div class="content__bottom">
+                          <span class="content__label"><?= __('Дивитися інгредієнти', 'bob'); ?></span>
+                          <button class="content__btn">
+                            <svg width="36" height="36">
+                              <use xlink:href="<?= get_template_directory_uri(); ?>/assets/img/sprite.svg#eye"></use>
+                            </svg>
+                          </button>
+                        </div>
+                      <?php else : ?>
+                        <p class="content__descr"><?= $subtitle; ?></p>
+                      <?php endif; ?>
                     </div>
-                  </div>
+
+                    <?php if ($long_ingredients) : ?>
+                      <div class="content__ingredients">
+                        <h3><?= __('Інгредієнти:', 'bob'); ?></h3>
+                        <p>
+                          <?= $subtitle; ?>
+                        </p>
+                      </div>
+                    </div>
                   <?php endif; ?>
-                </li> 
-              <?php endforeach; ?>                         
+                </li>
+              <?php endforeach; ?>
             </ul>
-          <?php endif; ?>          
+          <?php endif; ?>
         </div>
       <?php endforeach; ?>
     </div>
   <?php endif; ?>
 
-  <?php if ( $button && !empty($button) && isset($button['text']) && !empty($button['text']) && isset($button['link']) && !empty($button['link']) ) : ?>
+  <?php if ($button && !empty($button) && isset($button['text']) && !empty($button['text']) && isset($button['link']) && !empty($button['link'])) : ?>
     <a href="<?= $button['link']; ?>" class="tabs__link btn"><?= $button['text']; ?></a>
-  <?php endif; ?>  
+  <?php endif; ?>
 </div>
