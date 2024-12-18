8a233303f4957eaf8242b35353d92fee733566ac
Elena Kiseljova <e.a.kiseljova@gmail.com>
2024-12-18 18:39:44 +0200

    add: three new product templates

---
+++ single-products-big-christmas-box.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Template Name: Big Christmas box
+ * Template Post Type: products
+ */
+?>
+
+<?php
+get_header();
+?>
+
+<?php
+get_template_part('templates/big-christmas-box/top');
+
+// Check value exists.
+if (have_rows('content')) :
+
+  // Loop through rows.
+  while (have_rows('content')) : the_row();
+
+    // Case: composition layout.
+    if (get_row_layout() == 'composition') :
+      get_template_part('templates/big-christmas-box/composition');
+
+    endif;
+
+  // End loop.
+  endwhile;
+
+// No value.
+else :
+// Do something...
+endif;
+?>
+
+<?php
+get_footer();
+?>
