b191ff7a678f894de53132f34b3988efc1b5b9da
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-01-28 16:39:48 +0200

    fix: toggle bg (front)

--- front/#src/js/background-fill.js
+++ front/#src/js/background-fill.js
@@ -10,31 +10,24 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   const checkScrollPosition = () => {
-    if (window.scrollY > 500) {
-      if (
-        composition &&
-        window.screenY > composition.getBoundingClientRect().top &&
-        composition &&
-        document.body.dataset.colorBottom
-      ) {
+    if (!document.body.classList.contains("observed")) {
+      if (window.scrollY > 500) {
         if (
-          compositionNext &&
-          compositionNext.dataset.color &&
-          window.screenY > compositionNext.getBoundingClientRect().top
+          composition &&
+          window.screenY > composition.getBoundingClientRect().top &&
+          document.body.dataset.colorBottom
         ) {
-          document.body.style.backgroundColor = compositionNext.dataset.color;
-        } else {
           // Если фон "волны" в футере задан (data-color-bottom) и отличается от фона первого экрана (data-color) и пользователь проскролил composition секцию
           document.body.style.backgroundColor =
             document.body.dataset.colorBottom;
+        } else {
+          // Если пользователь проскролил ниже первого экрана - переключаем фон страницы на белый
+          document.body.style.backgroundColor = "#fff";
         }
       } else {
-        // Если пользователь проскролил ниже первого экрана - переключаем фон страницы на белый
-        document.body.style.backgroundColor = "#fff";
+        // Если пользователь находится в пределах первого экрана - переключаем фон страницы на дефолтный цвет этой страницы (data-color)
+        document.body.style.backgroundColor = document.body.dataset.color;
       }
-    } else {
-      // Если пользователь находится в пределах первого экрана - переключаем фон страницы на дефолтный цвет этой страницы (data-color)
-      document.body.style.backgroundColor = document.body.dataset.color;
     }
   };
 
@@ -43,4 +36,31 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("scroll", () => {
     checkScrollPosition();
   });
+
+  // Init Observer
+  const options = {};
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      // Toggle class when element is in view
+      entry.target.classList.toggle("enter", entry.isIntersecting);
+
+      if (
+        entry.target.classList.contains("enter") &&
+        entry.target.dataset.color
+      ) {
+        document.body.classList.add("observed");
+
+        document.body.style.backgroundColor = compositionNext.dataset.color;
+      } else {
+        document.body.classList.remove("observed");
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  if (compositionNext) {
+    observer.observe(compositionNext);
+  }
 });
