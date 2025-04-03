df7bf224868abbbb8c4852eb731c85aff996fb49
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-04-03 17:08:41 +0300

    upd: color and mb in Eat&Play

--- templates/eat&play/composition.php
+++ templates/eat&play/composition.php
@@ -1,41 +1,40 @@
-<?php 
-  $slides = get_sub_field( 'slides' ) ?? [];
+<?php
+$slides = get_sub_field('slides') ?? [];
 
-  $bob_text_left = get_sub_field( 'bob_text_left' ) ?? '';
+$bob_text_left = get_sub_field('bob_text_left') ?? '';
 
-  $image = get_sub_field( 'image' ) ?? '';
-  $bob_image_text = get_sub_field( 'bob_image_text' ) ?? '';
+$image = get_sub_field('image') ?? '';
+$bob_image_text = get_sub_field('bob_image_text') ?? '';
 ?>
 <section class="composition play">
- <div class="composition__wrapper">
+  <div class="composition__wrapper">
     <div class="container">
       <div class="composition__inner">
-        <?php if ( $slides && !empty($slides) && is_array($slides) && !is_wp_error( $slides ) ) : ?>
+        <?php if ($slides && !empty($slides) && is_array($slides) && !is_wp_error($slides)) : ?>
           <div class="play__slider swiper">
             <div class="play__wrapper swiper-wrapper">
               <?php foreach ($slides as $key => $slide) : ?>
-                <?php 
-                  $background = $slide['background'] ?? '';
-                  $background_image = $slide['background_image'] ?? '';
+                <?php
+                $background = $slide['background'] ?? '';
+                $background_image = $slide['background_image'] ?? '';
 
-                  $title = $slide['title'] ?? '';
-                  $text = $slide['text'] ?? '';
+                $title = $slide['title'] ?? '';
+                $text = $slide['text'] ?? '';
 
-                  $list = $slide['list'] ?? [];
+                $list = $slide['list'] ?? [];
 
-                  $button = $slide['button'] ?? [];
+                $button = $slide['button'] ?? [];
                 ?>
                 <div class="play__slide swiper-slide" <?= !empty($background) ? 'style="background-color: ' . $background . ';"' : ''; ?>>
-                  <?php if ( !empty($background_image) ) : ?>
+                  <?php if (!empty($background_image)) : ?>
                     <div class="play__banner">
                       <picture>
                         <img
                           src="<?= $background_image; ?>"
-                          alt="<?= get_bloginfo( 'name' ); ?>"
-                        />
-                      </picture>      
-                    </div>                    
-                  <?php endif; ?>    
+                          alt="<?= get_bloginfo('name'); ?>" />
+                      </picture>
+                    </div>
+                  <?php endif; ?>
 
                   <div class="play__head">
                     <h2 class="title"><?= $title; ?></h2>
@@ -43,21 +42,21 @@
                     <?= $text; ?>
                   </div>
 
-                  <?php if ( $list && !empty($list) && is_array($list) && !is_wp_error( $list ) ) : ?>
+                  <?php if ($list && !empty($list) && is_array($list) && !is_wp_error($list)) : ?>
                     <div class="play__toys toys">
-                      <ul class="toys__list">
+                      <ul class="toys__list" style="margin-bottom: 0;">
                         <?php foreach ($list as $key => $toy) : ?>
-                          <?php 
-                            $toy_title = $toy['title'] ?? '';
-                            $toy_description = $toy['description'] ?? '';
-                            $toy_image = $toy['image'] ?? '';
+                          <?php
+                          $toy_title = $toy['title'] ?? '';
+                          $toy_description = $toy['description'] ?? '';
+                          $toy_image = $toy['image'] ?? '';
 
-                            $toy_type = $toy['type'] ?? 'short';
+                          $toy_type = $toy['type'] ?? 'short';
                           ?>
                           <?php if ($toy_type === 'short') : ?>
                             <li class="toys__item">
                               <div class="toys__image">
-                                <img src="<?= $toy_image; ?>" alt="<?= get_bloginfo( 'name' ); ?>" />
+                                <img src="<?= $toy_image; ?>" alt="<?= get_bloginfo('name'); ?>" />
                               </div>
                               <h3><?= $toy_title; ?></h3>
                               <p><?= $toy_description; ?></p>
@@ -67,14 +66,14 @@
                               <div class="toys__wrapper">
                                 <div class="toys__front">
                                   <div class="toys__image">
-                                    <img src="<?= $toy_image; ?>" alt="<?= get_bloginfo( 'name' ); ?>" />
+                                    <img src="<?= $toy_image; ?>" alt="<?= get_bloginfo('name'); ?>" />
                                   </div>
                                   <h3><?= $toy_title; ?></h3>
                                   <div class="toys__bottom">
-                                    <span class="toys__label"><?= __( 'Дивитися', 'bob' ); ?></span>
+                                    <span class="toys__label"><?= __('Дивитися', 'bob'); ?></span>
                                     <button class="toys__btn">
                                       <svg width="36" height="36">
-                                        <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#eye"></use>
+                                        <use xlink:href="<?= get_template_directory_uri(); ?>/assets/img/sprite.svg#eye"></use>
                                       </svg>
                                     </button>
                                   </div>
@@ -86,31 +85,31 @@
                                 </div>
                               </div>
                             </li>
-                          <?php endif; ?>                        
-                        <?php endforeach; ?>                    
+                          <?php endif; ?>
+                        <?php endforeach; ?>
                       </ul>
                     </div>
-                    <?php if ( $button && !empty($button) && isset($button['text']) && !empty($button['text']) && isset($button['link']) && !empty($button['link']) ) : ?>
+                    <?php if ($button && !empty($button) && isset($button['text']) && !empty($button['text']) && isset($button['link']) && !empty($button['link'])) : ?>
                       <a href="<?= $button['link']; ?>" class="play__link btn"><?= $button['text']; ?></a>
-                    <?php endif; ?>  
-                  <?php endif; ?>                
+                    <?php endif; ?>
+                  <?php endif; ?>
                 </div>
-              <?php endforeach; ?>            
+              <?php endforeach; ?>
             </div>
             <div class="play__btns">
               <button class="play__btn play__btn--prev btn">
                 <svg width="12" height="8">
-                  <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#arrow-down-white"></use>
+                  <use xlink:href="<?= get_template_directory_uri(); ?>/assets/img/sprite.svg#arrow-down-white"></use>
                 </svg>
               </button>
               <button class="play__btn play__btn--next btn">
                 <svg width="12" height="8">
-                  <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#arrow-down-white"></use>
+                  <use xlink:href="<?= get_template_directory_uri(); ?>/assets/img/sprite.svg#arrow-down-white"></use>
                 </svg>
               </button>
-            </div>         
+            </div>
           </div>
-        <?php endif; ?>      
+        <?php endif; ?>
       </div>
     </div>
     <div class="play__left">
@@ -119,16 +118,16 @@
           <?= $bob_text_left; ?>
         </p>
       </div>
-      <img src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/snail-sings.svg" alt="<?= get_bloginfo( 'name' ); ?>" />
+      <img src="<?= get_template_directory_uri(); ?>/assets/img/icons/snail-sings.svg" alt="<?= get_bloginfo('name'); ?>" />
     </div>
   </div>
 
   <div class="composition__block">
     <div class="play__center">
-      <img src="<?= $image; ?>" alt="<?= get_bloginfo( 'name' ); ?>" />
+      <img src="<?= $image; ?>" alt="<?= get_bloginfo('name'); ?>" />
 
       <div class="play__snail play__snail--box">
-        <img src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/snail-sings.svg" alt="<?= get_bloginfo( 'name' ); ?>" />
+        <img src="<?= get_template_directory_uri(); ?>/assets/img/icons/snail-sings.svg" alt="<?= get_bloginfo('name'); ?>" />
         <div class="play__snail-text">
           <p><?= $bob_image_text; ?></p>
         </div>
@@ -136,43 +135,35 @@
     </div>
     <img
       class="icon play__fruit play__fruit--1"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/pear-original.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
+      src="<?= get_template_directory_uri(); ?>/assets/img/icons/pear-original.svg"
+      alt="<?= get_bloginfo('name'); ?>" />
     <img
       class="icon play__fruit play__fruit--2"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/strawberry-2.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
+      src="<?= get_template_directory_uri(); ?>/assets/img/icons/strawberry-2.svg"
+      alt="<?= get_bloginfo('name'); ?>" />
     <img
       class="icon play__fruit play__fruit--3"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/apple-piece.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
+      src="<?= get_template_directory_uri(); ?>/assets/img/icons/apple-piece.svg"
+      alt="<?= get_bloginfo('name'); ?>" />
     <img
       class="icon play__fruit play__fruit--4"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/cherry.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />    
+      src="<?= get_template_directory_uri(); ?>/assets/img/icons/cherry.svg"
+      alt="<?= get_bloginfo('name'); ?>" />
     <img
       class="icon play__fruit play__fruit--5"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/blueberry.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
+      src="<?= get_template_directory_uri(); ?>/assets/img/icons/blueberry.svg"
+      alt="<?= get_bloginfo('name'); ?>" />
     <img
       class="icon play__fruit play__fruit--6"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/pear.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
+      src="<?= get_template_directory_uri(); ?>/assets/img/icons/pear.svg"
+      alt="<?= get_bloginfo('name'); ?>" />
     <img
       class="icon play__fruit play__fruit--7"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/orange-2.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
+      src="<?= get_template_directory_uri(); ?>/assets/img/icons/orange-2.svg"
+      alt="<?= get_bloginfo('name'); ?>" />
     <img
       class="icon play__fruit play__fruit--8"
-      src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/banana.svg"
-      alt="<?= get_bloginfo( 'name' ); ?>"
-    />
-  </div>  
+      src="<?= get_template_directory_uri(); ?>/assets/img/icons/banana.svg"
+      alt="<?= get_bloginfo('name'); ?>" />
+  </div>
 </section>
--- templates/eat&play/top.php
+++ templates/eat&play/top.php
@@ -1,19 +1,19 @@
-<?php 
-  $top_bob_text = get_field( 'top_bob_text' ) ?? '';
+<?php
+$top_bob_text = get_field('top_bob_text') ?? '';
 
-  $button = get_field( 'button' );
+$button = get_field('button');
 
-  $images = get_field( 'images' ) ?? [];
+$images = get_field('images') ?? [];
 ?>
 <section class="top play">
   <div class="container">
     <div class="top__inner">
-      <div class="top__head top__head--dark">
-          <?php 
-            the_content(  );
-          ?>
+      <div class="top__head">
+        <?php
+        the_content();
+        ?>
         <div class="play__snail">
-          <img src="<?= get_template_directory_uri(  ); ?>/assets/img/eat&play/snail.png" alt="<?= get_bloginfo( 'name' ); ?>" />
+          <img src="<?= get_template_directory_uri(); ?>/assets/img/eat&play/snail.png" alt="<?= get_bloginfo('name'); ?>" />
           <div class="play__snail-text">
             <p>
               <?= $top_bob_text; ?>
@@ -22,68 +22,59 @@
         </div>
       </div>
 
-      <?php if ( $button && !empty($button) && is_array($button) && !is_wp_error( $button ) ) : ?>
-        <?php 
-          $title = $button['title'] ?? '';
+      <?php if ($button && !empty($button) && is_array($button) && !is_wp_error($button)) : ?>
+        <?php
+        $title = $button['title'] ?? '';
 
-          $link = $button['link'] && !empty($button['link']) ? $button['link'] : '#tabs';
+        $link = $button['link'] && !empty($button['link']) ? $button['link'] : '#tabs';
         ?>
-        
+
         <?php if (!empty($title)) : ?>
           <a href="<?= $link; ?>" class="btn"><?= $title; ?></a>
-        <?php endif; ?>        
+        <?php endif; ?>
       <?php endif; ?>
     </div>
-  </div>  
+  </div>
 
-  <?php if ( $images && !empty($images) && is_array($images) && !is_wp_error( $images ) ) : ?>
+  <?php if ($images && !empty($images) && is_array($images) && !is_wp_error($images)) : ?>
     <?php foreach ($images as $key => $image) : ?>
       <img
         class="icon play__image play__image--<?= $key + 1; ?>"
         src="<?= $image; ?>"
-        alt="<?= get_bloginfo( 'name' ); ?>"
-      />
+        alt="<?= get_bloginfo('name'); ?>" />
     <?php endforeach; ?>
   <?php endif; ?>
 
   <img
     class="icon play__image play__image--3"
-    src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/orange-2.svg"
-    alt="<?= get_bloginfo( 'name' ); ?>"
-  />
+    src="<?= get_template_directory_uri(); ?>/assets/img/icons/orange-2.svg"
+    alt="<?= get_bloginfo('name'); ?>" />
   <img
     class="icon play__image play__image--4"
-    src="<?= get_template_directory_uri(  ); ?>/assets/img/rolls/roll_3.png"
-    alt="<?= get_bloginfo( 'name' ); ?>"
-  />
+    src="<?= get_template_directory_uri(); ?>/assets/img/rolls/roll_3.png"
+    alt="<?= get_bloginfo('name'); ?>" />
   <img
     class="icon play__image play__image--5"
-    src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/strawberry-2.svg"
-    alt="<?= get_bloginfo( 'name' ); ?>"
-  />
+    src="<?= get_template_directory_uri(); ?>/assets/img/icons/strawberry-2.svg"
+    alt="<?= get_bloginfo('name'); ?>" />
   <img
     class="icon play__image play__image--6"
-    src="<?= get_template_directory_uri(  ); ?>/assets/img/rolls/roll_2.png"
-    alt="<?= get_bloginfo( 'name' ); ?>"
-  />
+    src="<?= get_template_directory_uri(); ?>/assets/img/rolls/roll_2.png"
+    alt="<?= get_bloginfo('name'); ?>" />
   <img
     class="icon play__image play__image--7"
-    src="<?= get_template_directory_uri(  ); ?>/assets/img/eat&play/snail-sleigh.png"
-    alt="<?= get_bloginfo( 'name' ); ?>"
-  />
+    src="<?= get_template_directory_uri(); ?>/assets/img/eat&play/snail-sleigh.png"
+    alt="<?= get_bloginfo('name'); ?>" />
   <img
     class="icon play__image play__image--8"
-    src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/pear.svg"
-    alt="<?= get_bloginfo( 'name' ); ?>"
-  />
+    src="<?= get_template_directory_uri(); ?>/assets/img/icons/pear.svg"
+    alt="<?= get_bloginfo('name'); ?>" />
   <img
     class="icon play__image play__image--9"
-    src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/orange-big.svg"
-    alt="<?= get_bloginfo( 'name' ); ?>"
-  />
+    src="<?= get_template_directory_uri(); ?>/assets/img/icons/orange-big.svg"
+    alt="<?= get_bloginfo('name'); ?>" />
   <img
     class="icon play__image play__image--10"
-    src="<?= get_template_directory_uri(  ); ?>/assets/img/icons/raspberries.svg"
-    alt="<?= get_bloginfo( 'name' ); ?>"
-  />
+    src="<?= get_template_directory_uri(); ?>/assets/img/icons/raspberries.svg"
+    alt="<?= get_bloginfo('name'); ?>" />
 </section>
